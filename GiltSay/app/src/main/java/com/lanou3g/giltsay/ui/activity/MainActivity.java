0e48b6e0a24955b81f3a88d80adeafaa3da90005
qiansha <pangmeilnyx@163.com>
2016-09-08 21:39:04 +0800

    无

--- GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/activity/MainActivity.java
+++ GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lanou3g.giltsay.R;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AbsBaseActivity {
 
