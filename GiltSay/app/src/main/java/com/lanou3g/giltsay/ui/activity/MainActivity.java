80dadbc1f00f19d553118a3e61a2f764b7f82eb3
qiansha <pangmeilnyx@163.com>
2016-09-08 21:58:31 +0800

    少部分的Mian定义

--- GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/activity/MainActivity.java
+++ GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/activity/MainActivity.java
@@ -4,9 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lanou3g.giltsay.R;
+import com.lanou3g.giltsay.ui.fragment.ClassFragment;
+import com.lanou3g.giltsay.ui.fragment.HomePageFragment;
+import com.lanou3g.giltsay.ui.fragment.ListFragment;
+import com.lanou3g.giltsay.ui.fragment.PersonFragment;
+
 import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AbsBaseActivity {
+    private HomePageFragment homePageFragment;
+    private ListFragment listFragment;
+    private ClassFragment classFragment;
+    private PersonFragment personFragment;
 
 
     @Override
