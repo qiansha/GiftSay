4fbaf34443aa11974b159af8c02b1c5ebccb2813
qiansha <pangmeilnyx@163.com>
2016-09-11 18:06:13 +0800

    完成一级个人页面，为精选Fragment画布局

--- GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/fragment/PersonFragment.java
+++ GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/fragment/PersonFragment.java
@@ -1,11 +1,25 @@
 package com.lanou3g.giltsay.ui.fragment;
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.view.ViewPager;
+
 import com.lanou3g.giltsay.R;
+import com.lanou3g.giltsay.ui.adapter.PagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dllo on 16/9/8.
  */
 public class PersonFragment extends AbsBaseFragment{
+    private static final int myColor = 0XFFFF0033;
+    private TabLayout personTl;
+    private ViewPager personVp;
+    private List<Fragment>datas;
     @Override
     protected int setLayout() {
         return R.layout.fragment_person;
@@ -13,11 +27,24 @@ public class PersonFragment extends AbsBaseFragment{
 
     @Override
     protected void initViews() {
+        personTl = byView(R.id.person_tl);
+        personVp = byView(R.id.person_vp);
+
 
     }
 
     @Override
     protected void initDatas() {
+        datas = new ArrayList<>();
+        datas.add(new PersonTabGuidesFragment());
+        datas.add(new PersonTabSingleFragment());
+        PagerAdapter pagerAdapter = new PagerAdapter(getChildFragmentManager(),datas);
+        personVp.setAdapter(pagerAdapter);
+        personTl.setupWithViewPager(personVp);
+        personTl.setTabTextColors(Color.BLACK,Color.BLACK);
+        personTl.setSelectedTabIndicatorColor(myColor);
+        personTl.getTabAt(0).setText("攻略");
+        personTl.getTabAt(1).setText("单品");
 
     }
 }
