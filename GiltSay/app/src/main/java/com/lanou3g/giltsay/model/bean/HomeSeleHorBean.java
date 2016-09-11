42d05be63e645e12565fc9a4f2076946e0c682e4
qiansha <pangmeilnyx@163.com>
2016-09-11 19:36:14 +0800

    wu

---
+++ GiltSay/app/src/main/java/com/lanou3g/giltsay/model/bean/HomeSeleHorBean.java
@@ -0,0 +1,25 @@
+package com.lanou3g.giltsay.model.bean;
+
+import android.widget.ImageView;
+
+/**
+ * Created by dllo on 16/9/11.
+ */
+public class HomeSeleHorBean {
+    private int img;
+
+    public HomeSeleHorBean(int img) {
+        this.img = img;
+    }
+
+    public HomeSeleHorBean() {
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
--- GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/fragment/HomePageSelectedFragment.java
+++ GiltSay/app/src/main/java/com/lanou3g/giltsay/ui/fragment/HomePageSelectedFragment.java
@@ -1,11 +1,24 @@
 package com.lanou3g.giltsay.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.ListView;
+
 import com.lanou3g.giltsay.R;
+import com.lanou3g.giltsay.model.bean.HomeSeleHorBean;
+import com.lanou3g.giltsay.ui.adapter.HomePageSelectedHorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dllo on 16/9/9.
  */
 public class HomePageSelectedFragment extends AbsBaseFragment{
+    private ListView homeSelHorListView;
+    private HomePageSelectedHorAdapter homePageSelectedHorAdapter;
+    private List<HomeSeleHorBean>datas;
     @Override
     protected int setLayout() {
         return R.layout.fragment_homepage_selected;
@@ -14,10 +27,25 @@ public class HomePageSelectedFragment extends AbsBaseFragment{
     @Override
     protected void initViews() {
 
+        homeSelHorListView = byView(R.id.homepage_selected_horizontal_list);
+
+
     }
 
     @Override
     protected void initDatas() {
 
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        datas = new ArrayList<>();
+        homePageSelectedHorAdapter = new HomePageSelectedHorAdapter(getActivity());
+        homePageSelectedHorAdapter.setDatas(datas);
+        homeSelHorListView.setAdapter(homePageSelectedHorAdapter);
+        super.onActivityCreated(savedInstanceState);
+        Log.d("HomePageSelectedFragmen", "....");
     }
 }
